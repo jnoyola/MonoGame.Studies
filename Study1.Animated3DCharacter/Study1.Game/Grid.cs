6e19753b42b6ed8979b16ca618f072fc46cd2dd5
Jonathan Noyola <noyolajonathan@gmail.com>
2026-03-29 10:57:55 -0400

    [1.3] small optimizations

--- Study1.Animated3DCharacter/Study1.Game/Grid.cs
+++ Study1.Animated3DCharacter/Study1.Game/Grid.cs
@@ -6,37 +6,49 @@ namespace Study1.Game;
 public class Grid
 {
     private readonly BasicEffect _effect;
-    private readonly VertexPositionColor[] _vertices;
+    private readonly VertexBuffer _vertexBuffer;
+    private readonly int _primitiveCount;
 
     public Grid(GraphicsDevice graphicsDevice, uint width, Color? color = null)
     {
-        _effect = new BasicEffect(graphicsDevice);
-        _effect.VertexColorEnabled = true;
+        _effect = new BasicEffect(graphicsDevice)
+        {
+            VertexColorEnabled = true,
+            World = Matrix.Identity
+        };
 
         var vertexColor = color ?? Color.Black;
-        _vertices = new VertexPositionColor[(width + 1) * 2 * 2];
+        var vertices = new VertexPositionColor[(width + 1) * 2 * 2];
         var half = (float)width / 2;
         for (int i = 0; i <= width; ++i)
         {
-            _vertices[4 * i + 0] = new VertexPositionColor(new Vector3(-half + i, 0, -half), vertexColor);
-            _vertices[4 * i + 1] = new VertexPositionColor(new Vector3(-half + i, 0, half), vertexColor);
-            _vertices[4 * i + 2] = new VertexPositionColor(new Vector3(-half, 0, -half + i), vertexColor);
-            _vertices[4 * i + 3] = new VertexPositionColor(new Vector3(half, 0, -half + i), vertexColor);
+            vertices[4 * i + 0] = new VertexPositionColor(new Vector3(-half + i, 0, -half), vertexColor);
+            vertices[4 * i + 1] = new VertexPositionColor(new Vector3(-half + i, 0, half), vertexColor);
+            vertices[4 * i + 2] = new VertexPositionColor(new Vector3(-half, 0, -half + i), vertexColor);
+            vertices[4 * i + 3] = new VertexPositionColor(new Vector3(half, 0, -half + i), vertexColor);
         }
+
+        _vertexBuffer = new VertexBuffer(graphicsDevice, typeof(VertexPositionColor), vertices.Length, BufferUsage.WriteOnly);
+        _vertexBuffer.SetData(vertices);
+
+        _primitiveCount = vertices.Length / 2;
     }
 
-    public Matrix WorldMatrix { get; set; } = Matrix.Identity;
+    public Matrix WorldMatrix
+    {
+        get => _effect.World;
+        set => _effect.World = value;
+    }
 
     public void Draw(Matrix cameraView, Matrix cameraProjection)
     {
-        _effect.World = WorldMatrix;
         _effect.View = cameraView;
         _effect.Projection = cameraProjection;
-
+        _effect.GraphicsDevice.SetVertexBuffer(_vertexBuffer);
         for (int effectPassIndex = 0; effectPassIndex < _effect.CurrentTechnique.Passes.Count; ++effectPassIndex)
         {
             _effect.CurrentTechnique.Passes[effectPassIndex].Apply();
-            _effect.GraphicsDevice.DrawUserPrimitives(PrimitiveType.LineList, _vertices, 0, _vertices.Length / 2);
+            _effect.GraphicsDevice.DrawPrimitives(PrimitiveType.LineList, 0, _primitiveCount);
         }
     }
 }
